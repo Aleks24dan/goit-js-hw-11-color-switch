07d0da383319a048cbf301dcd6ee1cbb7d7a710b
Aleks24dan <aleks24dan@gmail.com>
2021-07-20 06:54:58 +0200

    12

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,47 @@
-import colors from "./hex.js";
-const body = document.querySelector('body');
-const startBtn = document.querySelector('[data-action="start"]');
-const stopBtn = document.querySelector('[data-action="stop"]');
-let COLORS_LENGTH = colors.length;
-let randomIntegerFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-};
-let intervalID = null;
+const colors= [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+];
+const refs = {
+    body : document.querySelector('body'),
+    startBtn : document.querySelector('[data-action="start"]'),
+    stopBtn : document.querySelector('[data-action="stop"]'),
+}
 
-startBtn.addEventListener('click', onStartBtnClik);
-stopBtn.addEventListener('click', onStopBtnClik);
+let COLORS_LENGTH = colors.length;
+
+
+let intervalID = null;
+let isBtnRun = false;
+
+refs.startBtn.addEventListener('click', onStartBtnClik);
+refs.stopBtn.addEventListener('click', onStopBtnClik);
 
 function onStartBtnClik() {
-    intervalID = setInterval(chengeBodyColor, 1000,)
-
+    if (isBtnRun) {
+        return;
+    }
+    isBtnRun = true;
+    refs.startBtn.disabled = true;
+    intervalID = setInterval(chengeBodyColor, 1000,);
+   
 }
 
 function chengeBodyColor() {
-    let randomColor = randomIntegerFromInterval(0, COLORS_LENGTH)
-    body.style.backgroundColor = colors[randomColor];
+    let randomColor = randomIntegerFromInterval(1, COLORS_LENGTH)
+    refs.body.style.backgroundColor = colors[randomColor];
 }
 
 function onStopBtnClik() {
     clearInterval(intervalID);
+    isBtnRun = false;
+    refs.startBtn.disabled = false;
 }
+
+function randomIntegerFromInterval(min, max){
+    return Math.floor(Math.random() * (max - min + 1) + min);
+};
